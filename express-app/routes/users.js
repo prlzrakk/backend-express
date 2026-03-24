57a5912801a519d2824dc648229e8eaa6d4ea262
Ilona Kazantseva <Ilona.Kazantseva@urfu.me>
2026-03-24 19:21:51 +0500

    господи...

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,33 +1,33 @@
 const express = require('express');
-const { useId } = require('react');
 const router = express.Router();
 
+const users = [{
+    "id": 1,
+    "name": "Илона"
+}, {
+    "id": 2,
+    "name": "Лёша"
+}];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({items: [{
-      "id": 1,
-      "name": "Илона"
-   }, {
-      "id": 2,
-      "name": "Лёша"
-   }]});
+router.get('/', function (req, res, next) {
+    res.json({items: users});
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     const newUser = req.body;
+    newUser.id = users.length > 0 ? users.length + 1 : 1;
+    users.push(newUser);
     return res.status(201).json(newUser);
 })
 
-router.get('/', function(req, res, next){
-  const users = req.body.items;
-  const UserId = req.params;
-  console.log(UserId);
-  if (users["id"].includes(UserId)){
-    return res.send(users[UserId]);
-  }
-  else{
-    res.status(403);
-  }
+router.get('/:id', function (req, res, next) {
+    const id = parseInt(req.params.id, 10);
+    const user = users.find(u => u.id === id);
+    if (user) {
+        return res.json(user);
+    }
+    return res.status(404);
 })
 
 module.exports = router;
